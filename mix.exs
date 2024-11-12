6fdc5aeafe5d22353253b11633e9248b659b2186
Keith Brings <keith.brings@noizu.com>
2024-11-13 06:58:27 +0700

    vsn bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SmartToken.MixProject do
   def project do
     [
       app: :smart_token,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.15",
       package: package(),
       deps: deps(),
